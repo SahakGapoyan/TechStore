9f84e6093048bc41b92bad317671c022b9de21c1
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-11 19:18:58 +0400

    Add CartItem

---
+++ TechStore/TechStore.Data/Entity/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.Data.Entity
+{
+    public class CartItem:BaseEntity
+    {
+        public decimal CurrentPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedAt { get; set; } = DateTime.Now;
+        public Product Product { get; set; } = default!;
+        public int ProductId { get; set; }
+        public decimal SubTotal => Quantity * Product.Price;
+    }
+}
