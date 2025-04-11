a21f0f88179c6bf6853994f946fb0e0afba1c0dd
Sahak <sahak.gapoyan02@mail.ru>
2025-04-11 18:29:17 +0400

    Add Tv Product.

---
+++ TechStore/TechStore.Data/Entity/Tv.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.Data.Entity
+{
+    public class Tv : Product
+    {
+        public string ScreenSize { get; set; }
+        public string? PanelType { get; set; }
+        public bool IsSmartTv { get; set; }
+    }
+}
