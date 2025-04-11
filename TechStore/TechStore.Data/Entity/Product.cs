edbc85bc569161de46b1902e125f43b7b71cc4ea
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-11 17:32:11 +0400

    Add Product Entity

---
+++ TechStore/TechStore.Data/Entity/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.Data.Entity
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public bool IsAvailable { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+        
+    }
+}
