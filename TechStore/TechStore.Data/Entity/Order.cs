a6f49dfc37845f43d37a444f2c366a673a08099c
Sahak <sahak.gapoyan02@mail.ru>
2025-04-11 19:35:57 +0400

    Add Order,OrderItem.

---
+++ TechStore/TechStore.Data/Entity/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Enums;
+
+namespace TechStore.Data.Entity
+{
+    public class Order : BaseEntity
+    {
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string? ShippingAddress { get; set; }
+        public IEnumerable<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
