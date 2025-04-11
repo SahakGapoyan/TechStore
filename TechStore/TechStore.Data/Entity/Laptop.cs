8c776efc8ffc1dfec1a90fa779e85f447f914676
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-11 18:59:24 +0400

    Add laptop

--- TechStore/TechStore.Data/Entity/Laptop.cs
+++ TechStore/TechStore.Data/Entity/Laptop.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TechStore.Data.Entity
 {
-    public class SmartPhone : Product
+    public class Laptop : Product
     {
         public Ram Ram { get; set; } = default!;
         public int RamId { get; set; }
         public Memory Memory { get; set; } = default!;
         public int MemoryId { get; set; }
         public OS OS { get; set; } = default!;
-        public int OSId { get; set; }
-        public string ScreenSize { get; set; } = default!;
+        public int OsId { get; set; }
         public string Processor { get; set; } = default!;
-        public int BatteryCapacity { get; set; }
-        public int CameraMegaPixel { get; set; }
-        public bool Has5G { get; set; }
-        public bool IsWaterResistant { get; set; }
-        public bool Wifi { get; set; }
+        public string GPU { get; set; } = default!;
+        public string ScreenSize { get; set; } = default!;
+        public int BatteryLifeInHours { get; set; }
+        public bool HasTouchScreen { get; set; }
+        public bool HasFingerprintSensor { get; set; }
+
     }
 }
