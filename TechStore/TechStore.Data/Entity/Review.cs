9efa1008477a65a3e42f1424f9fc93e25ce76746
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-11 19:33:18 +0400

    Add Review

--- TechStore/TechStore.Data/Entity/Review.cs
+++ TechStore/TechStore.Data/Entity/Review.cs
@@ -8,6 +8,8 @@ namespace TechStore.Data.Entity
 {
     public class Review:BaseEntity
     {
-
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
