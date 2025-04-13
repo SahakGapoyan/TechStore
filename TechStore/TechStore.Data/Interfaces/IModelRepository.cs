cf5c1e31325ecc1eb5e7534da54219b61fd5c6c4
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-13 22:55:32 +0400

    Add IModelRepository

---
+++ TechStore/TechStore.Data/Interfaces/IModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface IModelRepository
+    {
+        Task<IEnumerable<Model>> GetModels(CancellationToken token = default);
+        Task<Model?> GetModel(int modelId, CancellationToken token = default);
+        Task AddModel(Model model, CancellationToken token = default);
+        Task Update(Model model);
+        Task Delete(Model model);
+    }
+}
