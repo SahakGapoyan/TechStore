56a6c3b95b6b1e3015d7c2f3305ad96309172077
Sahak <sahak.gapoyan02@mail.ru>
2025-04-13 20:50:02 +0400

    Add CartItem Repository.

---
+++ TechStore/TechStore.Data/Interfaces/ICartItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface ICartItemRepository
+    {
+        Task<IEnumerable<CartItem>> GetCartItemsByUserId(int userId, CancellationToken token = default);
+        Task AddCartItem(CartItem cartItem, CancellationToken token = default);
+        Task UpdateCartItem(CartItem cartItem);
+        Task DeleteCartItem(CartItem cartItem);
+    }
+}
