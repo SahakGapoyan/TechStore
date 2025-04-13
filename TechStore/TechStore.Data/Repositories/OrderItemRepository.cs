c2014d9d7b28fa6a4f0fb46e7cca292cbac38813
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-13 22:19:09 +0400

    Add OrderRepository

---
+++ TechStore/TechStore.Data/Repositories/OrderItemRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.DbContext;
+using TechStore.Data.Entity;
+using TechStore.Data.Interfaces;
+
+namespace TechStore.Data.Repositories
+{
+    public class OrderItemRepository : IOrderItemRepository
+    {
+        private readonly TechStoreDbContext _context;
+
+        public OrderItemRepository(TechStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddOrderItem(OrderItem orderItem, CancellationToken token = default)
+        {
+            await _context.OrderItems.AddAsync(orderItem, token);
+        }
+
+        public async Task<OrderItem?> GetOrderItemById(int orderItemId, CancellationToken token = default)
+        {
+            return await _context.OrderItems.FirstOrDefaultAsync(o => o.Id == orderItemId);
+        }
+
+        public async Task<IEnumerable<OrderItem>> GetOrderItemsByUserId(int userId, CancellationToken token = default)
+        {
+            return await _context.OrderItems
+                 .Include(o => o.Order)
+                 .Where(o => o.Order.UserId == userId)
+                 .ToListAsync(token);
+        }
+    }
+}
