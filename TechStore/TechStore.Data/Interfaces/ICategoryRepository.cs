4bad4ac78fa7a4b91e040bb4681862055adc0690
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-13 21:08:25 +0400

    Add ICategoryRepository Interface

---
+++ TechStore/TechStore.Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories(CancellationToken toke = default);
+        Task<Category?> GetCategory(int id, CancellationToken token = default);
+        Task AddCategory(Category category, CancellationToken toke = default);
+        Task UpdateCategory(Category category);
+        Task DeleteCategory(Category category);
+    }
+}
