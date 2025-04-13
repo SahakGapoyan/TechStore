e219c48a36d4c3679b9aca758084fdf601d653c6
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-13 23:07:55 +0400

    Add ModelRepository

--- TechStore/TechStore.Data/Interfaces/IModelRepository.cs
+++ TechStore/TechStore.Data/Interfaces/IModelRepository.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
 
 namespace TechStore.Data.Interfaces
 {
