a720484f1f1a3cd80b1db556daf0eb0d8dbaf768
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-14 16:05:39 +0400

    Add Memory Repository

---
+++ TechStore/TechStore.Data/Interfaces/IMemoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface IMemoryRepository
+    {
+        Task<IEnumerable<Memory>> GetMemories(CancellationToken token = default);
+        Task<Memory?> GetMemoryById(int memoryId, CancellationToken token = default);
+        Task AddMemory(Memory memory, CancellationToken token = default);
+        Task UpdateMemory(Memory memory);
+        Task DeleteMemory(Memory memory);
+    }
+}
