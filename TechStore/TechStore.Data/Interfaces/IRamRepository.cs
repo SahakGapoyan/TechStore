13930224865693bddf6e819fc7f805e7b3868b74
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-14 13:35:54 +0400

    Add RamRepository

---
+++ TechStore/TechStore.Data/Interfaces/IRamRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface IRamRepository
+    {
+        Task<Ram?> GetRamById(int ramId, CancellationToken token = default);
+        Task AddRam(Ram ram, CancellationToken token = default);
+        Task UpdateRam(Ram ram);
+        Task DeleteRam(Ram ram);
+    }
+}
---
+++ TechStore/TechStore.Data/Repositories/RamRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.DbContext;
+using TechStore.Data.Entity;
+using TechStore.Data.Interfaces;
+
+namespace TechStore.Data.Repositories
+{
+    public class RamRepository : IRamRepository
+    {
+        private readonly TechStoreDbContext _context;
+
+        public RamRepository(TechStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddRam(Ram ram, CancellationToken token = default)
+        {
+            await _context.Rams.AddAsync(ram,token);
+        }
+
+        public async Task DeleteRam(Ram ram)
+        {
+            _context.Rams.Remove(ram);
+        }
+
+        public async Task<Ram?> GetRamById(int ramId, CancellationToken token = default)
+        {
+            return await _context.Rams.FirstOrDefaultAsync(r => r.Id == ramId, token);
+        }
+
+        public async Task UpdateRam(Ram ram)
+        {
+            _context.Rams.Update(ram);
+        }
+    }
+}
