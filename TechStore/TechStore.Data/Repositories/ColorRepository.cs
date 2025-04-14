023d8176e9d28f3585b6c446265dcedc52baf99a
Sahak <sahak.gapoyan02@mail.ru>
2025-04-14 13:36:27 +0400

    Add Color Repository,

---
+++ TechStore/TechStore.Data/Repositories/ColorRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.DbContext;
+using TechStore.Data.Entity;
+using TechStore.Data.Interfaces;
+
+namespace TechStore.Data.Repositories
+{
+    public class ColorRepository : IColorRepository
+    {
+        private readonly TechStoreDbContext _context;
+
+        public ColorRepository(TechStoreDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddColor(Color color, CancellationToken token = default)
+        {
+            await _context.Colors.AddAsync(color, token);
+        }
+
+        public async Task DeleteColor(Color color)
+        {
+            _context.Colors.Remove(color);
+        }
+
+        public async Task<Color?> GetColorById(int colorId, CancellationToken token = default)
+        {
+            return await _context.Colors.FirstOrDefaultAsync(c => c.Id == colorId, token);
+        }
+
+        public async Task UpdateColor(Color color)
+        {
+            _context.Colors.Update(color);
+        }
+    }
+}
