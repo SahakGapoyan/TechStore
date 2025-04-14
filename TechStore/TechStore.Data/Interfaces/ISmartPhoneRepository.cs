dca127db1b3e2cad761125408afc8b57c4c4b737
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-14 18:44:31 +0400

    Add SmartPhoneRepository

---
+++ TechStore/TechStore.Data/Interfaces/ISmartPhoneRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface ISmartPhoneRepository : IProductRepository<SmartPhone>
+    {
+        Task<IEnumerable<SmartPhone>> GetSmartPhonesByRamId(int ramId, CancellationToken token = default);
+        Task<IEnumerable<SmartPhone>> GetSmartPhonesByMemoryId(int memoryId, CancellationToken token = default);
+        Task<IEnumerable<SmartPhone>> GetSmartPhonesByOSID(int OSId, CancellationToken token = default);
+    }
+}
---
+++ TechStore/TechStore.Data/Repositories/SmartPhoneRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.DbContext;
+using TechStore.Data.Entity;
+using TechStore.Data.Interfaces;
+
+namespace TechStore.Data.Repositories
+{
+    public class SmartPhoneRepository:ProductRepository<SmartPhone>,ISmartPhoneRepository
+    {
+        public SmartPhoneRepository(TechStoreDbContext context):base(context)
+        {
+        }
+
+        public async Task<IEnumerable<SmartPhone>> GetSmartPhonesByMemoryId(int memoryId, CancellationToken token = default)
+        {
+            return await _dbSet.Where(s => s.MemoryId == memoryId).ToListAsync(token);
+        }
+
+        public async Task<IEnumerable<SmartPhone>> GetSmartPhonesByOSID(int OSId, CancellationToken token = default)
+        {
+            return await _dbSet.Where(s => s.OSId == OSId).ToListAsync(token);
+        }
+
+        public async Task<IEnumerable<SmartPhone>> GetSmartPhonesByRamId(int ramId, CancellationToken token = default)
+        {
+            return await _dbSet.Where(s => s.RamId == ramId).ToListAsync(token);
+        }
+    }
+}
