47ea79b598f5ce8f75fbe629a0ee7bca273ebe73
Sahak <sahak.gapoyan02@mail.ru>
2025-04-14 16:11:15 +0400

    Add OS Repository.

---
+++ TechStore/TechStore.Data/Interfaces/IOSRepsoitory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.Data.Interfaces
+{
+    public interface IOSRepsoitory
+    {
+        Task<IEnumerable<OS>> GetOSs(CancellationToken token=default);
+        Task<OS?> GetOSById(int osId, CancellationToken token=default);
+        Task AddOS(OS oS, CancellationToken token = default);
+        Task UpdateOS(OS oS);
+        Task DeleteOS(OS oS);
+    }
+}
