7caf8d2117a54262c3273fdf7b82c3c6736e55a6
Sahak <sahak.gapoyan02@mail.ru>
2025-04-15 15:45:42 +0400

    Add BrandDto's

---
+++ TechStore/TechStore.BLL/DtoModels/Brand/BrandUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+
+namespace TechStore.BLL.DtoModels.Brand
+{
+    public class BrandUpdateDto
+    {
+        public string? Name { get; set; }
+    }
+}
