31a15426ffc88c98a8452a01eca18b01298b4e64
Sahak <sahak.gapoyan02@mail.ru>
2025-04-15 18:45:37 +0400

    Add Os Service.

---
+++ TechStore/TechStore.BLL/Interfaces/IOSService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels;
+using TechStore.BLL.DtoModels.OS;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Interfaces
+{
+    public interface IOSService
+    {
+        Task<IEnumerable<OSDto>> GetOSs(CancellationToken token = default);
+        Task<OSDto?> GetOSById(int osId, CancellationToken token = default);
+        Task AddOS(OSAddDto oSAddDto, CancellationToken token = default);
+        Task<Result> UpdateOS(int OsId, OSUpdateDto oSUpdateDto, CancellationToken token);
+        Task<Result> DeleteOS(int OsId,  CancellationToken token);
+    }
+}
