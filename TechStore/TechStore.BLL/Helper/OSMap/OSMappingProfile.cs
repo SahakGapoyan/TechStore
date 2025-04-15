7ee2f815af098040bdf2dfd553330d4a788f57bc
Sahak <sahak.gapoyan02@mail.ru>
2025-04-15 18:45:19 +0400

    Add Os Map Profile

---
+++ TechStore/TechStore.BLL/Helper/OSMap/OSMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.OS;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Helper.OSMap
+{
+    public class OSMappingProfile : Profile
+    {
+        public OSMappingProfile()
+        {
+            CreateMap<OS, OSDto>();
+            CreateMap<OSAddDto, OS>();
+        }
+    }
+}
