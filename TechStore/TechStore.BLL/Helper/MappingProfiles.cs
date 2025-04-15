00aa81993105720a287921b4ea132027822da9c4
Sahak <sahak.gapoyan02@mail.ru>
2025-04-15 15:46:06 +0400

    Add AutoMapper for Brand.

--- TechStore/TechStore.BLL/Helper/MappingProfiles.cs
+++ TechStore/TechStore.BLL/Helper/MappingProfiles.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Brand;
+using TechStore.Data.Entity;
 
 namespace TechStore.BLL.Helper
 {
@@ -11,7 +13,8 @@ namespace TechStore.BLL.Helper
     {
         public MappingProfiles()
         {
-            
+            CreateMap<Brand, BrandDto>();
+            CreateMap<BrandAddDto, Brand>();
         }
     }
 }
