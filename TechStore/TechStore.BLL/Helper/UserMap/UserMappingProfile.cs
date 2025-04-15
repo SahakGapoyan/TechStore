6e1f072fef6b7f584d63e23c179f4a3b7ca98f7e
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-15 15:56:35 +0400

    Add UserMap

---
+++ TechStore/TechStore.BLL/Helper/UserMap/UserMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.User;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Helper.UserMap
+{
+    class UserMappingProfile:Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<User, UserAddDto>().ReverseMap();
+        }
+    }
+}
