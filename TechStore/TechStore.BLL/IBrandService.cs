965fb981e904926d5999c14a5350d7f3a5246eca
Sahak <sahak.gapoyan02@mail.ru>
2025-04-15 17:58:23 +0400

    Add Brand Service.

---
+++ TechStore/TechStore.BLL/IBrandService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels;
+using TechStore.BLL.DtoModels.Brand;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL
+{
+    public interface IBrandService
+    {
+        Task<IEnumerable<BrandDto>> GetBrands(CancellationToken token = default);
+        Task<BrandDto?> GetBrand(int brandId, CancellationToken token = default);
+        Task AddBrand(BrandAddDto brandAddDto, CancellationToken token = default);
+        Task<Result> UpdateBrand(int brandId,BrandUpdateDto brandUpdateDto,CancellationToken token=default);
+        Task<Result> DeleteBrand(int brandId,CancellationToken token=default);
+    }
+}
