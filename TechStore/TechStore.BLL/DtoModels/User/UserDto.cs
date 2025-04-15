d28d7e0867e1d2474de34a5a1b7c3b5b4b943a45
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-15 16:11:49 +0400

    Add UserDto

---
+++ TechStore/TechStore.BLL/DtoModels/User/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+
+namespace TechStore.BLL.DtoModels.User
+{
+    public class UserDto:BaseDto
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
