8c3fd9b05140c2a2565ba1c3e29645fa2949a751
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-16 15:54:34 +0400

    Add ColorDto

---
+++ TechStore/TechStore.BLL/DtoModels/Color/ColorDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+
+namespace TechStore.BLL.DtoModels.Color
+{
+    public class ColorDto:BaseDto
+    {
+        public string Name { get; set; } = default!;
+    }
+}
---
+++ TechStore/TechStore.BLL/DtoModels/Color/ColorUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.BLL.DtoModels.Color
+{
+    public class ColorUpdateDto
+    {
+        public string? Name { get; set; }
+    }
+}
