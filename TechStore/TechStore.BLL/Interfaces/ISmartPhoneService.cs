61916e2680ba8ad69232dab2dc143936c006c8fa
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-16 15:19:36 +0400

    Add SmartPhoneService

---
+++ TechStore/TechStore.BLL/Interfaces/ISmartPhoneService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.SmartPhone;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Interfaces
+{
+    public interface ISmartPhoneService:IProductService<SmartPhone,SmartPhoneDto,SmartPhoneAddDto,SmartPhoneUpdateDto>
+    {
+        Task<IEnumerable<SmartPhoneDto>> GetSmartPhonesByRamId(int ramId, CancellationToken token = default);
+        Task<IEnumerable<SmartPhoneDto>> GetSmartPhonesByMemoryId(int memoryId, CancellationToken token = default);
+        Task<IEnumerable<SmartPhoneDto>> GetSmartPhonesByOSID(int OSId, CancellationToken token = default);
+    }
+}
