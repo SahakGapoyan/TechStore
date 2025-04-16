943b23cd64328880b3f09ab1f8cb6694df2037bf
Sahak <sahak.gapoyan02@mail.ru>
2025-04-16 15:42:52 +0400

    Add Tv Map.

---
+++ TechStore/TechStore.BLL/Helper/TvMap/TvMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Tv;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Helper.TvMap
+{
+    public class TvMappingProfile : Profile
+    {
+        public TvMappingProfile()
+        {
+            CreateMap<Tv, TvDto>();
+            CreateMap<TvAddDto, Tv>();
+        }
+    }
+}
