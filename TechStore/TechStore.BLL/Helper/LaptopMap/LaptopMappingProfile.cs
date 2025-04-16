b7dca08e709cb6b4903a12cc515792999f8302ba
Sahak <sahak.gapoyan02@mail.ru>
2025-04-16 15:18:25 +0400

    Add Laptop Map.

---
+++ TechStore/TechStore.BLL/Helper/LaptopMap/LaptopMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Laptop;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Helper.LaptopMap
+{
+    public class LaptopMappingProfile : Profile
+    {
+        public LaptopMappingProfile()
+        {
+            CreateMap<Laptop, LaptopDto>();
+            CreateMap<LaptopAddDto, Laptop>();
+        }
+    }
+}
