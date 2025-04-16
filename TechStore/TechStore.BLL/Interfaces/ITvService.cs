00fb8363ec7fe6bf6a807d47c19d4785c7e91081
Sahak <sahak.gapoyan02@mail.ru>
2025-04-16 15:43:11 +0400

    Add TvService.

---
+++ TechStore/TechStore.BLL/Interfaces/ITvService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Tv;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Interfaces
+{
+    public interface ITvService : IProductService<Tv, TvDto, TvAddDto, TvUpdatedto>
+    {
+
+    }
+}
