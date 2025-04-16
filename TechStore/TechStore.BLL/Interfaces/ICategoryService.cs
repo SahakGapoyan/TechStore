e34ae3edea57e9359177cea75ab6444a9bb9317e
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-16 13:23:28 +0400

    Add CategoryService

---
+++ TechStore/TechStore.BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels;
+using TechStore.BLL.DtoModels.Category;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDto>> GetCategories(CancellationToken token = default);
+        Task<CategoryDto?> GetCategory(int id, CancellationToken token = default);
+        Task AddCategory(CategoryAddDto categoryAddDto, CancellationToken token = default);
+        Task<Result> UpdateCategory(int categoryId,CategoryUpdateDto categoryUpdateDto, CancellationToken token = default);
+        Task<Result> DeleteCategory(int categoryId, CancellationToken token = default);
+    }
+}
