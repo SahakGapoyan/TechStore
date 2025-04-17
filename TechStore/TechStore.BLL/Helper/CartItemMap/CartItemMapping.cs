d11474fb1595b54dc47fcff8a2e213cb24191f26
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-17 14:02:43 +0400

    Add CartItemMap

---
+++ TechStore/TechStore.BLL/Helper/CartItemMap/CartItemMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.CartItem;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Helper.CartItemMap
+{
+    public class CartItemMapping:Profile
+    {
+        public CartItemMapping()
+        {
+            CreateMap<CartItem, CartItemDto>();
+            CreateMap<CartItem, CartItemAddDto>().ReverseMap();
+        }
+    }
+}
