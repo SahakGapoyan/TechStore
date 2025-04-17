7b5281c574f1bb521173a58cc42684fe7e659fb2
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-17 19:30:05 +0400

    Add OrderServive

---
+++ TechStore/TechStore.BLL/Interfaces/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels;
+using TechStore.BLL.DtoModels.Order;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderDto>> GetOrdersByUserId(int userId, CancellationToken token = default);
+        Task<OrderDto?> GetOrder(int orderId, CancellationToken token = default);
+        Task AddOrder(OrderAddDto orderAddDto, CancellationToken token = default);
+        Task<Result> UpdateOrder(int orderId,OrderUpdateDto orderUpdateDto,CancellationToken token=default);
+    }
+}
