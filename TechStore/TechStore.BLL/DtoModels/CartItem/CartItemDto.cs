646a2551a25f4cc088fa2cef07b09af3ef323140
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-17 14:02:19 +0400

    Add CartItemDto

---
+++ TechStore/TechStore.BLL/DtoModels/CartItem/CartItemDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+
+namespace TechStore.BLL.DtoModels.CartItem
+{
+    public class CartItemDto:BaseDto
+    {
+        public decimal CurrentPrice;
+        public int Quantity { get; set; }
+        public DateTime AddedAt { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public decimal SubTotal;
+    }
+}
---
+++ TechStore/TechStore.BLL/DtoModels/CartItem/CartItemUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechStore.BLL.DtoModels.CartItem
+{
+    public class CartItemUpdateDto
+    {
+        public decimal? CurrentPrice;
+        public int? Quantity { get; set; }
+        public int? ProductId { get; set; }
+        public int? UserId { get; set; }
+        public decimal? SubTotal;
+    }
+}
