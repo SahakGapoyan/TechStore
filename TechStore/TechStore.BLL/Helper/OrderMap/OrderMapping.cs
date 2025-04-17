c20c9e84e283b3a6497a4d30b9b316f2bb10b7da
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-17 19:29:43 +0400

    Add OrderMapping

---
+++ TechStore/TechStore.BLL/Helper/OrderMap/OrderMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Order;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.Helper.OrderMap
+{
+    public class OrderMapping:Profile
+    {
+        public OrderMapping()
+        {
+            CreateMap<Order, OrderDto>();
+            CreateMap<Order, OrderAddDto>().ReverseMap();
+        }
+    }
+}
