e59a369115d6a3ee9d30a2a2ff037c1d66dc6f93
Sahak <sahak.gapoyan02@mail.ru>
2025-04-17 18:27:09 +0400

    Add OrderItem Dto's.

---
+++ TechStore/TechStore.BLL/DtoModels/OrderItem/OrderItemAddDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.DtoModels.OrderItem
+{
+    public class OrderItemAddDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ TechStore/TechStore.BLL/DtoModels/OrderItem/OrderItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+using TechStore.Data.Entity;
+
+namespace TechStore.BLL.DtoModels.OrderItem
+{
+    public class OrderItemDto:BaseDto
+    {
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
