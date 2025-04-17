37f7064513298d4c82eee3ddb9ebcc68e4a5fe49
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-17 19:28:46 +0400

    Add OrderDto

---
+++ TechStore/TechStore.BLL/DtoModels/Order/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+using TechStore.Data.Enums;
+
+namespace TechStore.BLL.DtoModels.Order
+{
+    public class OrderDto:BaseDto
+    {
+        public DateTime OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string? ShippingAddress { get; set; }
+    }
+}
---
+++ TechStore/TechStore.BLL/DtoModels/Order/OrderUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.Data.Enums;
+
+namespace TechStore.BLL.DtoModels.Order
+{
+    public class OrderUpdateDto
+    {
+        public OrderStatus? Status { get; set; }
+        public string? ShippingAddress { get; set; }
+    }
+}
