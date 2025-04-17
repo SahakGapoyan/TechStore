bcc08aae0b22e72f9c554acf112cadee5a1c29b3
Sahak <sahak.gapoyan02@mail.ru>
2025-04-17 14:33:05 +0400

    Add ModelService.

---
+++ TechStore/TechStore.BLL/Interfaces/IModelService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels;
+using TechStore.BLL.DtoModels.Model;
+
+namespace TechStore.BLL.Interfaces
+{
+    public interface IModelService
+    {
+        Task<IEnumerable<ModelDto>> GetModels(CancellationToken token = default);
+        Task<ModelDto?> GetModel(int modelId, CancellationToken token = default);
+        Task AddModel(ModelAddto modelAddDto, CancellationToken token = default);
+        Task<Result> Update(int modelId, ModelUpdateDto modelUpdateDto, CancellationToken token = default);
+        Task<Result> Delete(int modelId, CancellationToken token = default);
+    }
+}
---
+++ TechStore/TechStore.BLL/Services/ModelService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels;
+using TechStore.BLL.DtoModels.Enums;
+using TechStore.BLL.DtoModels.Model;
+using TechStore.BLL.Interfaces;
+using TechStore.Data.Entity;
+using TechStore.Data.Interfaces;
+
+namespace TechStore.BLL.Services
+{
+    public class ModelService : IModelService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public ModelService(IUnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+        public async Task AddModel(ModelAddto modelAddDto, CancellationToken token = default)
+        {
+            var model = _mapper.Map<Model>(modelAddDto);
+            await _uow.ModelRepository.AddModel(model, token);
+            await _uow.SaveAsync(token);
+        }
+
+        public async Task<Result> Delete(int modelId, CancellationToken token = default)
+        {
+            var model = await _uow.ModelRepository.GetModel(modelId, token);
+
+            if (model == null)
+                return Result.Error(ErrorType.NotFound);
+
+            await _uow.ModelRepository.Delete(model);
+            await _uow.SaveAsync(token);
+
+            return Result.Ok("Successfully deleted");
+        }
+
+        public async Task<ModelDto?> GetModel(int modelId, CancellationToken token = default)
+        {
+            return _mapper.Map<ModelDto>(await _uow.ModelRepository.GetModel(modelId, token));
+        }
+
+        public async Task<IEnumerable<ModelDto>> GetModels(CancellationToken token = default)
+        {
+            return _mapper.Map<List<ModelDto>>(await _uow.ModelRepository.GetModels(token));
+        }
+
+        public async Task<Result> Update(int modelId, ModelUpdateDto modelUpdateDto, CancellationToken token = default)
+        {
+            var model = await _uow.ModelRepository.GetModel(modelId, token);
+
+            if (model == null)
+                return Result.Error(ErrorType.NotFound);
+
+            model.Name = modelUpdateDto.Name ?? model.Name;
+            model.Stock = modelUpdateDto.Stock ?? model.Stock;
+            model.AnnouncementYear = modelUpdateDto.AnnouncementYear ?? model.AnnouncementYear;
+
+            await _uow.ModelRepository.Update(model);
+            await _uow.SaveAsync(token);
+
+            return Result.Ok("Successfully updated.");
+        }
+    }
+}
