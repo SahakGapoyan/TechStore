44a5576f5400d9248dfc828d34a1270be0a83205
Sahak <sahak.gapoyan02@mail.ru>
2025-04-17 14:32:27 +0400

    Add Model Dto's.

---
+++ TechStore/TechStore.BLL/DtoModels/Model/ModelDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechStore.BLL.DtoModels.Base;
+
+namespace TechStore.BLL.DtoModels.Model
+{
+    public class ModelDto : BaseDto
+    {
+        public string Name { get; set; } = default!;
+        public int AnnouncementYear { get; set; }
+        public int Stock { get; set; }
+    }
+}
