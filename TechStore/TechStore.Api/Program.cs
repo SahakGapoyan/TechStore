d7e354797bf2c15544ad097b46c8230c8c633081
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-18 15:35:18 +0400

    Add DI

--- TechStore/TechStore.Api/Program.cs
+++ TechStore/TechStore.Api/Program.cs
@@ -1,4 +1,12 @@
 
+using Microsoft.AspNetCore.Mvc;
+using TechStore.BLL.DtoModels.Laptop;
+using TechStore.BLL.DtoModels.Product;
+using TechStore.BLL.DtoModels.SmartPhone;
+using TechStore.BLL.DtoModels.Tv;
+using TechStore.BLL.Interfaces;
+using TechStore.BLL.Services;
+using TechStore.Data.Entity;
 using TechStore.Data.Interfaces;
 using TechStore.Data.Repositories;
 
@@ -21,6 +29,26 @@ namespace TechStore.Api
 
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+            builder.Services.AddScoped<IBrandService, BrandService>();
+            builder.Services.AddScoped<ICartItemService, CartItemService>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IColorService, ColorService>();
+            builder.Services.AddScoped<ILaptopService, LaptopService>();
+            builder.Services.AddScoped<IMemoryService, MemoryService>();
+            builder.Services.AddScoped<IModelService, ModelService>();
+            builder.Services.AddScoped<IOrderItemService, OrderItemService>();
+            builder.Services.AddScoped<IOrderService, OrderService>();
+            builder.Services.AddScoped<IOSService, OSService>();
+            builder.Services.AddScoped<IProductService<SmartPhone, SmartPhoneDto, SmartPhoneAddDto, SmartPhoneUpdateDto>,
+                ProductService<SmartPhone, SmartPhoneDto, SmartPhoneAddDto, SmartPhoneUpdateDto>>();
+            builder.Services.AddScoped<IProductService<Laptop, LaptopDto, LaptopAddDto, LaptopUpdateDto>,
+                ProductService<Laptop, LaptopDto, LaptopAddDto, LaptopUpdateDto>>();
+            builder.Services.AddScoped<IProductService<Tv, TvDto, TvAddDto, TvUpdateDto>,
+                ProductService<Tv, TvDto, TvAddDto, TvUpdateDto>>();
+            builder.Services.AddScoped<IRamService, RamService>();
+            builder.Services.AddScoped<ISmartPhoneService, SmartPhoneService>();
+            builder.Services.AddScoped<ITvService, TvService>();
+            builder.Services.AddScoped<IUserService, UserService>();
 
             var app = builder.Build();
 
@@ -30,7 +58,6 @@ namespace TechStore.Api
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseAuthorization();
 
 
