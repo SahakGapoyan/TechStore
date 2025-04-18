18f5b9494daa847fbfd7b2686f6043664d8ff719
Sahak <sahak.gapoyan02@mail.ru>
2025-04-18 20:01:27 +0400

    Add Brand Controller.

---
+++ TechStore/TechStore.Api/Controllers/BrandsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechStore.BLL.DtoModels.Brand;
+using TechStore.BLL.DtoModels.Enums;
+using TechStore.BLL.Interfaces;
+
+namespace TechStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<BrandDto>>> GetBrands(CancellationToken token)
+        {
+            return Ok(await _brandService.GetBrands(token));
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<ActionResult<BrandDto>> GetBrandById([FromRoute] int id, CancellationToken token)
+        {
+            var brand = await _brandService.GetBrand(id, token);
+
+            if (brand == null)
+                return NotFound();
+
+            return Ok(brand);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateBrand(BrandAddDto brandAddDto, CancellationToken token)
+        {
+            await _brandService.AddBrand(brandAddDto, token);
+
+            return Ok("Successfully created.");
+        }
+
+        [HttpPut("id/{id}")]
+        public async Task<ActionResult> UpdateBrand([FromRoute] int id, [FromBody] BrandUpdateDto brandUpdateDto, CancellationToken token)
+        {
+            var result = await _brandService.UpdateBrand(id, brandUpdateDto, token);
+
+            if (!result.Success)
+            {
+                if (result.ErrorType == ErrorType.NotFound)
+                    return NotFound();
+            }
+
+            return Ok(result.Message);
+        }
+
+        [HttpDelete("id/{id}")]
+        public async Task<ActionResult> DeleteBrand([FromRoute] int id, CancellationToken token)
+        {
+            var result = await _brandService.DeleteBrand(id, token);
+
+            if (!result.Success)
+            {
+                if (result.ErrorType == ErrorType.NotFound)
+                    return NotFound();
+            }
+
+            return Ok(result.Message);
+        }
+
+    }
+}
