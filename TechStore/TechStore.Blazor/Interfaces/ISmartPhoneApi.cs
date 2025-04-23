67f44e4f6db22fbcae67a41460b3cbf0117b658f
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-04-23 17:21:12 +0400

    Add SmartPhoneApi

---
+++ TechStore/TechStore.Blazor/Interfaces/ISmartPhoneApi.cs
@@ -0,0 +1,13 @@
+﻿using TechStore.Blazor.DtoModels.SmartPhone;
+
+namespace TechStore.Blazor.Interfaces
+{
+    public interface ISmartPhoneApi
+    {
+        Task<IEnumerable<SmartPhoneDto>> GetSmartPhones();
+        Task<SmartPhoneDto> GetSmartPhone(int id);
+        Task AddSmartPhone(SmartPhoneAddDto smartPhoneAddDto);
+        Task Update(int id,SmartPhoneUpdateDto smartPhoneUpdateDto);
+        Task Delete(int id);
+    }
+}
