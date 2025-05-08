c2d1fc31d546f9a221e0c40b99d0fcc9e1a6eebe
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-08 20:08:52 +0400

    Add Validation for OS

--- TechStore/TechStore.Blazor/DtoModels/OS/OSAddDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/OS/OSAddDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.OS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.OS
 {
     public class OSAddDto
     {
+        [Required(ErrorMessage = "Անունը պարտադիր է")]
         public string Name { get; set; } = default!;
     }
 }
--- TechStore/TechStore.Blazor/DtoModels/OS/OSUpdateDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/OS/OSUpdateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.OS
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.OS
 {
     public class OSUpdateDto
     {
+        [Required(ErrorMessage = "Անունը պարտադիր է")]
         public string? Name { get; set; }
     }
 }
