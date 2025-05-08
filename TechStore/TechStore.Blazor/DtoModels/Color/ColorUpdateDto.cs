10384fbd2345f82f8870d1b20a00409775ef57d5
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-08 19:07:45 +0400

    Add Validations for Color

--- TechStore/TechStore.Blazor/DtoModels/Color/ColorUpdateDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/Color/ColorUpdateDto.cs
@@ -1,8 +1,12 @@
-﻿namespace TechStore.Blazor.DtoModels.Color
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.Color
 {
     public class ColorUpdateDto
     {
+        [Required(ErrorMessage = "Անունը պարտադիր է")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Գույնի կոդը պարտադիր է")]
         public string? HexCode { get; set; }
     }
 }
