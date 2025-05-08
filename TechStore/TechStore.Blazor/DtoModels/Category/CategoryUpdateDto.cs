198bb93389a61319c1afc0037775f84ee2e3325a
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-08 19:07:11 +0400

    Add Validations for Category

--- TechStore/TechStore.Blazor/DtoModels/Category/CategoryUpdateDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/Category/CategoryUpdateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.Category
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.Category
 {
     public class CategoryUpdateDto
     {
+        [Required(ErrorMessage = "Անունը պարտադիր է")]
         public string? Name { get; set; }
         public string? Description { get; set; }
     }
