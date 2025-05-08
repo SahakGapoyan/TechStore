29f22a02e2de944be39cce26c89890e2b3113972
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-09 02:08:37 +0400

    Add ApiResult and ValidationError

---
+++ TechStore/TechStore.Blazor/DtoModels/Result/ApiResult.cs
@@ -0,0 +1,11 @@
+﻿namespace TechStore.Blazor.DtoModels.Result
+{
+    public class ApiResult<T>
+    {
+        public bool Success { get; set; }
+        public T Data { get; set; }
+        public List<ValidationError> ValidationErrors { get; set; }
+        public string ErrorMessage { get; set; }
+
+    }
+}
