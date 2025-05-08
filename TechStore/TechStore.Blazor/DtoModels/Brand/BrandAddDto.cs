edb2814c3843a165cd038c03e02499ccf21a785f
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-08 19:06:28 +0400

    Add Validations for Brand

--- TechStore/TechStore.Blazor/DtoModels/Brand/BrandAddDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/Brand/BrandAddDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.Brand
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.Brand
 {
     public class BrandAddDto
     {
+        [Required(ErrorMessage = "Անունը պարտադիր է")]
         public string Name { get; set; } = default!;
         public string ImageUrl { get; set; } = default!;
     }
--- TechStore/TechStore.Blazor/DtoModels/Brand/BrandUpdateDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/Brand/BrandUpdateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.Brand
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.Brand
 {
     public class BrandUpdateDto
     {
+        [Required(ErrorMessage = "Անունը պարտադիր է")]
         public string? Name { get; set; }
         public string? ImageUrl { get; set; } 
     }
