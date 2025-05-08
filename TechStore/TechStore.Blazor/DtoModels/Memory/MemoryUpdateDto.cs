48cccac5d70f31a1bac21ca3d729c27f340f3419
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-08 19:59:14 +0400

    Add Validations for Memory

--- TechStore/TechStore.Blazor/DtoModels/Memory/MemoryUpdateDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/Memory/MemoryUpdateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.Memory
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.Memory
 {
     public class MemoryUpdateDto
     {
+        [Required(ErrorMessage = "Ծավալը պարտադիր է")]
         public string? Size { get; set; }
     }
 }
