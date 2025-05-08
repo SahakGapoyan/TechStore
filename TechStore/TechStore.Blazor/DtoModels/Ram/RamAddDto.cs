7d485ad80cd995ceabb96d0ca482cdd8019890d0
aniavetisyan004 <120250905+aniavetisyan004@users.noreply.github.com>
2025-05-08 20:12:08 +0400

    Add Validations for Ram

--- TechStore/TechStore.Blazor/DtoModels/Ram/RamAddDto.cs
+++ TechStore/TechStore.Blazor/DtoModels/Ram/RamAddDto.cs
@@ -1,7 +1,10 @@
-﻿namespace TechStore.Blazor.DtoModels.Ram
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Blazor.DtoModels.Ram
 {
     public class RamAddDto
     {
+        [Required(ErrorMessage = "Ծավալը պարտադիր է")]
         public string Size { get; set; } = default!;
     }
 }
